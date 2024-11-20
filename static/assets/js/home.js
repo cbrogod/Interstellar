46dad3b02b06dd089ac10d0dff9d5426ac4723b8
nebula <154573357+cbrogod@users.noreply.github.com>
2024-11-19 21:03:09 -0600

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -11,7 +11,6 @@ if (
   !navigator.userAgent.includes("Firefox") &&
   localStorage.getItem("ab") === "true"
 ) {
-  const popup = open("about:blank", "_blank");
   if (!popup || popup.closed) {
     alert(
       "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.",
